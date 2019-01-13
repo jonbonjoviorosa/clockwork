7cadaade4219f2be5f1754a7067ac014bdf4725e
vhon orosa <jonbonjoviorosa@gmail.com>
2019-01-13 20:29:49 +0800

    Fixed Api Port

--- Clockwork.API/Program.cs
+++ Clockwork.API/Program.cs
@@ -14,12 +14,21 @@ namespace Clockwork.API
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+           .UseKestrel()
+           .UseContentRoot(Directory.GetCurrentDirectory())
+           .UseIISIntegration()
+           .UseStartup<Startup>()
+           .UseUrls("http://localhost:58093/")
+           .Build();
+
+            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
     }
 }
