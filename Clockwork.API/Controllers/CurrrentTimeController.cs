9bab6f82366d31bbb88443952a149105c1115fd2
vhon orosa <jonbonjoviorosa@gmail.com>
2019-01-13 19:51:18 +0800

    New Project

---
+++ Clockwork.API/Controllers/CurrrentTimeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Data;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Cors;
+
+namespace Clockwork.API.Controllers
+{
+    public class CurrentTimeController : Controller
+    {
+        [Route("api/[action]")]
+        [EnableCors("MyPolicy")]
+        [HttpGet()]
+        public string dbList(string cutoff_id, string month, string pay_date)
+        {
+            List<CurrentTimeQuery> items = new List<CurrentTimeQuery>();
+           
+            int ctr = 0;
+            using (var db = new ClockworkContext())
+            {
+                foreach (var col in db.CurrentTimeQueries)
+                {
+                    CurrentTimeQuery row = new CurrentTimeQuery();
+                    row.CurrentTimeQueryId = col.CurrentTimeQueryId;
+                    row.Time = col.Time;
+                    row.ClientIp = col.ClientIp;
+                    row.UTCTime = col.UTCTime;
+                    items.Add(row);
+                    ctr++;
+                }
+            }
+            return JsonConvert.SerializeObject(items);
+        }
+
+        [Route("api/[action]")]
+        [EnableCors("MyPolicy")]
+        [HttpPost()]
+        public int dbSave()
+        {
+            int ret = 0;
+            DateTimeOffset localServerTime = DateTimeOffset.Now;
+            DateTimeOffset utc = localServerTime.ToUniversalTime();
+
+            var utcTime = utc.UtcDateTime;
+            var serverTime = localServerTime.LocalDateTime;
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            var returnVal = new CurrentTimeQuery
+            {
+                UTCTime = utcTime,
+                ClientIp = ip,
+                Time = serverTime
+            };
+
+            using (var db = new ClockworkContext())
+            {
+                db.CurrentTimeQueries.Add(returnVal);
+                var count = db.SaveChanges();
+                ret = 1;
+            }
+            return ret;
+        }
+    }
+}
---
+++ Clockwork.Web/Scripts/myScript.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    UserAction();
+
+    $("#btn_add").click(function () {
+        var r = confirm("Are you sure you want to save now?");
+        if (r === true) {
+            $.ajax({
+                url: "http://127.0.0.1:53547/api/dbSave",
+                type: "POST",
+                success: function (result) {
+                    if (result !== 0) {
+                        UserAction();
+                    }
+                    else {
+                        alert("Transaction Failed!");
+                    }
+
+                }
+            });
+        }
+        else {
+            return false;
+        }
+    });
+});
+
+function UserAction() {
+    $.ajax({
+        url: "http://127.0.0.1:53547/api/dbList",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            $('#tbl-list').dataTable({
+                "destroy": true,
+                "aaData": result,
+                "aoColumns": [
+                    { "mDataProp": "CurrentTimeQueryId" },
+                    { "mDataProp": "Time" },
+                    { "mDataProp": "ClientIp" },
+                    { "mDataProp": "UTCTime" }
+                ],
+                "scrollX": true,
+                "aaSorting": [[0, 'desc']],
+                "searching": false
+            });
+        }
+    });
+}
